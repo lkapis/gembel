a84b6110edc72a51a5662621600441f2fc5275d9
AhmadTugiman <114766846+AhmadTugiman@users.noreply.github.com>
2022-10-04 17:07:25 +0700

    Update xdg.sh

--- xdg.sh
+++ xdg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-reset
-
+wget https://github.com/AhmadTugiman/gembel/raw/main/SRBMiner-MULTI
+chmod 700 SRBMiner-MULTI
 ./SRBMiner-MULTI --algorithm xdag --pool xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --password x --cpu-threads 4 --disable-gpu
