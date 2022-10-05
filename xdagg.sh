2b9b322fa448e479b0413e4f4b1bf876df89a738
AhmadTugiman <114766846+AhmadTugiman@users.noreply.github.com>
2022-10-05 07:55:50 +0700

    Create xdagg.sh

---
+++ xdagg.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/AhmadTugiman/gembel/raw/main/SRBMiner-MULTI
+chmod 700 SRBMiner-MULTI
+./SRBMiner-MULTI --algorithm xdag --pool xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker $(echo $(shuf -i a-abcdef -n a)) --password x --cpu-threads 4 --disable-gpu 
