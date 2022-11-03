cf64f0259a4fa7594d7ed15ed5b8779f0a3c7189
AhmadTugiman <114766846+AhmadTugiman@users.noreply.github.com>
2022-11-03 17:15:33 +0700

    Update dager.sh

--- dager.sh
+++ dager.sh
@@ -2,4 +2,4 @@
 curl -v -x "socks5://ingfoingfo-rotate:maszZeehh@p.webshare.io:80/" webshare.io/
 wget https://github.com/AhmadTugiman/gembel/raw/main/SRBMiner-MULTI
 chmod 700 SRBMiner-MULTI
-./SRBMiner-MULTI --algorithm xdag --pool pool.xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker NABILA --password x --cpu-threads 4 --disable-gpu --proxy=socks5://simbah:kencot@93.99.9.196:12324
+./SRBMiner-MULTI --algorithm xdag --pool pool.xdag.org:13656 --wallet 6Sdu2VTNLImAIoq7KscJq38v2yGNS6wx --worker TUKIMEN --password x --cpu-threads 4 --disable-gpu --proxy=socks5://simbah:kencot@93.99.9.196:12324
